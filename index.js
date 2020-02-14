fb400e64d58285c2355a9fddc06fc0cf1c7f7298
nicballesteros <nicballesteros2@gmail.com>
2020-02-13 20:37:19 -0500

    commented index.js

--- index.js
+++ index.js
@@ -31,36 +31,33 @@ options = { //set up the parameters of the GET request
   }
 };
 
-//console.log(options.qs.since);
-
 let leftoverTradeData = [];
 
 function writeDataToFile(filename, writeData){ //generic data writer async
-  return new Promise((resolve, reject) => {
-    //console.log("hello world");
+  return new Promise((resolve, reject) => { //a long proccess so don't block other proccesses
     try {
       let from = writeData[0].time;
       let to = writeData[writeData.length - 1].time;
 
       writeData = { //store the
         from: from,
-        to: to,
+        to: to, //make json obj
         data: writeData
       };
       //console.log(writeData);
       writeData = JSON.stringify(writeData); //turn data passed in writeData into json string object
 
-      filename = './json/' + filename + '.json'; //make the filename
+      filename = __dirname + '/json/' + filename + '.json'; //make the filename
       console.log(filename);
       let writeStream = fs.createWriteStream(filename); //open a write stream, much more efficient for large files
 
-      writeStream.write(writeData);
+      writeStream.write(writeData); //write the data
 
-      writeStream.close();
+      writeStream.close(); //close writeStream
       resolve(filename); //tell Node that everything worked out.
     }
     catch (error) {
-      reject(error);
+      reject(error);//tell the promise something bad has happened
     }
   });
 }
@@ -68,16 +65,17 @@ function writeDataToFile(filename, writeData){ //generic data writer async
 let filesCreated = 0; //store how many files have been made.
 
 let ohlcData = []; //what the OHLC data will eventually be stored in
-let splitIntoMins = [];
+let splitIntoMins = []; //make a larger array of mins where each min is seperated from other mins
 
+/* Parse Data */
 function parseData(requestedData) { //parse the data coming from web
   return new Promise((resolve, reject) => {
-    //if (error) throw new Error(error);
-    //try {
-    let singleMin = [];
-    let json = requestedData;
-  //  writeDataToFile('test', requestedData).then(() => console.log("done")).catch(console.error("error 135"));
+
+    let singleMin = []; //init a single min of trade data
+    let json = requestedData; //make json the same as requested data
     let filetime = MINDATE - 60;
+
+    leftoverTradeData = []; //get rid of the last min of leftover data
 
     for (let i = 0; i < json.length; i++) {
       let date = json[i][2];
@@ -91,15 +89,15 @@ function parseData(requestedData) { //parse the data coming from web
         leftoverTradeData.push(json[i]);
       }
       else { //if the next min is within the last min of the file and has moved onto a new min;
-        MINDATE += 60;
-        splitIntoMins.push(singleMin);
-        singleMin = [];
-        singleMin.push(json[i]);
+        MINDATE += 60; //the min is full and now we iterate to next min
+        splitIntoMins.push(singleMin); //add to the larger array of mins
+        singleMin = []; //make the next min array empty
+        singleMin.push(json[i]); //start the next min array
       }
     }
     console.log("Minutes of OHLC data proccessed: " + splitIntoMins.length);
 
-    if(splitIntoMins.length > 730){
+    if(splitIntoMins.length > 730){ //make sure theres a lil more than we need
       let minSet = splitIntoMins.slice(0,720); //get the first 720 elements of the array
       splitIntoMins = splitIntoMins.slice(720, splitIntoMins.length); //delete the first 720 elements from the array
 
@@ -116,7 +114,7 @@ function parseData(requestedData) { //parse the data coming from web
         if(singleMin.length > 0) { //if there were trades that min
           open = singleMin[0][0]; //set open
           close = singleMin[singleMin.length - 1][0]; //set close
-          //console.log(open, "close ", close);
+
           high = open;
           low = open;
 
@@ -138,24 +136,26 @@ function parseData(requestedData) { //parse the data coming from web
           low = open;
         }
 
-        let ohlc = new OHLC(time.toISOString(), open, high, low, close);
-        ohlcData.push(ohlc.toObj());
-        fileOutputDate += 60000;
+        let ohlc = new OHLC(time.toISOString(), open, high, low, close); //make a new obj from class
+        ohlcData.push(ohlc.toObj()); //push the object
+        fileOutputDate += 60000; //add a min worth of milliseconds to the output date
       }
 
       filename = STARTDATE + 'F' + (filesCreated);
 
-      writeDataToFile(filename, ohlcData).then(r => {
+      writeDataToFile(filename, ohlcData).then(r => { //call the write file function async
           console.log("Wrote " + r + " to the filesystem");
           filesCreated += 1;
           ohlcData = [];
           console.log(ohlcData);
+          resolve("Parsed");
         }).catch(err => console.error(err));
     }
   });
 }
 
-function requestData(number) {
+/* Request Data */
+function requestData(number) { //request data
   console.log('since: ', options.qs.since / 1000000); //since does not corrispond to a date
   console.log("Enddate: ", ENDDATE);
   if(options.qs.since / 1000000 < ENDDATE && number < 120) {
@@ -167,19 +167,20 @@ function requestData(number) {
       options.qs.since = json.result.last;
       json = json.result.XXBTZUSD //get rid of everything that isnt data
 
-      console.log("GET <- since: " + options.qs.since);
+      console.log("GET <- since: " + options.qs.since); //output to the console
       console.log("API CALLS: " + (number + 1));
 
-      json = leftoverTradeData.concat(json);
+      json = leftoverTradeData.concat(json); //get the last little bit of data
 
-      let timeout = setTimeout(() => { //make a new call to the server for more data
-        number += 1;
-        requestData(number);
-      }, 6000);
-      parseData(json).then(() => {
-          console.log("Data Parsed");
+      let timeout = setTimeout(() => { //make a new call to the server for more data every 6000 milliseconds or 6 sec
+        number += 1; //keep track of how many times we've iterated thru the function
+        requestData(number); //call the func we in rn
+      }, 6000); //not guarenteed every 6 seconds, but min amount rather spent between api calls
+
+      parseData(json).then(r => {  //call the parse data async
+          console.log("Data " + r); //prints if works if written to a file;
         }).catch(err => console.error(err));
     });
   }
 }
-requestData(0);
+requestData(0); //start the call stack
